07e2c9e5246fee06ad54d89f1da2020a6b1c9bc9
Luke Day <ld227707@falmouth.ac.uk>
2020-05-28 23:42:33 +0100

    Added a fire and tear delegate method to the InputHandler, as well as two corresponding exposed KeyCode variables.

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    [Header("Input")]
     public List<InputKey> inputs = new List<InputKey>();
+    public KeyCode fireAbilityKey = KeyCode.E;
+    public KeyCode tearAbilityKey = KeyCode.Q;
+
+    #region delegates
+
     public delegate void OnKeyPressed(Vector2 dir);
     public OnKeyPressed onKeyPressed;
+    public delegate void OnFireAbility();
+    public OnFireAbility onFireAbility;
+    public delegate void OnTearAbility();
+    public OnTearAbility onTearAbility;
+
+    #endregion
+
     public PlayerMovement playerMovement;
+    public PlayerAbility playerAbility;
 
     private void Start()
     {
         playerMovement.AssignDelegate(this);
+        playerAbility.AssignDelegate(this);
     }
 
     private void Update()
@@ -22,8 +37,21 @@ public class InputHandler : MonoBehaviour
             if (Input.GetKeyDown(input.key))
             {
                 onKeyPressed?.Invoke(input.direction);
+                break;
             }
-        }    
+        }
+
+        if (Input.GetKeyDown(fireAbilityKey))
+        {
+            onFireAbility?.Invoke();
+            return;
+        }
+
+        if (Input.GetKeyDown(tearAbilityKey))
+        {
+            onTearAbility?.Invoke();
+            return;
+        }
     }
 }
 
