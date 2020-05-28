cf17c86c1b4696d50267970a3b7cf102ffbe377a
Luke Day <ld227707@falmouth.ac.uk>
2020-05-28 20:12:37 +0100

    Added an InputHandler, which uses delegates and a custom event system to invoke methods when a key is pressed.

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public List<InputKey> inputs = new List<InputKey>();
+    public delegate void OnKeyPressed(Vector2 dir);
+    public OnKeyPressed onKeyPressed;
+    public PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerMovement.AssignDelegate(this);
+    }
+
+    private void Update()
+    {
+        foreach (InputKey input in inputs)
+        {
+            if (Input.GetKeyDown(input.key))
+            {
+                onKeyPressed?.Invoke(input.direction);
+            }
+        }    
+    }
+}
+
+[System.Serializable]
+public class InputKey
+{
+    public KeyCode key;
+    public Vector2 direction;
+}
