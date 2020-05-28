391d9099dd223d807b3d41d985afe9699815cf6b
Luke Day <ld227707@falmouth.ac.uk>
2020-05-28 23:43:09 +0100

    Added a PlayerAbility script, which utilises the Fire and Tear delegate methods, created in the previous commit.

---
+++ Assets/Scripts/PlayerAbility.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbility : MonoBehaviour
+{
+    public void AssignDelegate(InputHandler inputHandler)
+    {
+        inputHandler.onFireAbility += FireAbility;
+        inputHandler.onTearAbility += TearAbility;
+    }
+
+    private void FireAbility()
+    {
+        Debug.Log("FIRE");
+    }
+
+    private void TearAbility()
+    {
+        Debug.Log("TEAR");
+    }
+}
