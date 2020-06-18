975668123f63e74bacae62a21d20ebe600c697e6
Luke Day <ld227707@falmouth.ac.uk>
2020-06-18 17:15:25 +0100

    Added a GoalTrigger script, which determines when the player reaches the end of the level.

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player") Debug.Log("Player completed the level.");
+        Time.timeScale = 0.1f;
+    }
+}
