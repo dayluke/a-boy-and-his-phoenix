06bf828c92233ca052036d09c498474ab8a05ffc
Luke Day <ld227707@falmouth.ac.uk>
2020-06-19 21:46:50 +0100

    Made a level select scene

---
+++ Assets/Scripts/Menu/LevelSelectHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectHandler : MonoBehaviour
+{
+    public Object menuScene;
+    
+    public void OnLevelClick(int index)
+    {
+        MenuHandler menu = GameObject.FindGameObjectWithTag("Menu").GetComponent<MenuHandler>();
+        string sceneToLoad = menu.levelScenes[index].name;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void OnBackClick()
+    {
+        SceneManager.LoadScene(menuScene.name);
+    }
+}
