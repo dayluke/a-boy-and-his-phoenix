503ffc1ddf0cb1373068fcf695d52449c1cb034a
Luke Day <ld227707@falmouth.ac.uk>
2020-06-25 16:43:58 +0100

    Added a custom cursor, and fixed the main menu buttons being selected (rather than pressed)

---
+++ Assets/Scripts/Menu/MenuButtonHandler.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButtonHandler : MonoBehaviour, ISelectHandler
+{
+    public void OnSelect(BaseEventData eventData) =>gameObject.GetComponent<Button>().onClick.Invoke();
+}
