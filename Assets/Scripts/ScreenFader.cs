c77b1037f7c63cf8bafaa57623e2e6dd64881bd6
Luke Day <ld227707@falmouth.ac.uk>
2020-06-25 23:49:07 +0100

    Added a screen fade to every scene (in and out)

---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+    public float fadeTime = 1;
+    public Animator animator;
+
+    public void Fade() => animator.SetTrigger("fade");
+    public void Fade(MonoBehaviour instance, string sceneToLoad) => instance.StartCoroutine(Fade(sceneToLoad));
+
+    private IEnumerator Fade(string sceneToLoad)
+    {
+        animator.SetTrigger("fade");    
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
